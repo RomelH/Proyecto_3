6113d118ee8725fdcfb1827cfba5ee6c7c3b19de
RomelH <romelehc@yahoo.com.ar>
2024-02-25 23:22:42 -0300

    Mejora funcionalidad Carrito

--- js/carrito.js
+++ js/carrito.js
@@ -9,7 +9,7 @@ let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
 const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
 const contenedorTotal = document.querySelector("#total");
 const botonComprar = document.querySelector("#carrito-acciones-comprar");
-
+const contadorProductosCarrito = document.querySelector("#contador-productos");
 
 function cargarProductosCarrito() {
     if (productosEnCarrito && productosEnCarrito.length > 0) {
@@ -20,7 +20,7 @@ function cargarProductosCarrito() {
         contenedorCarritoComprado.classList.add("disabled");
     
         contenedorCarritoProductos.innerHTML = "";
-    
+        
         productosEnCarrito.forEach(producto => {
     
             const div = document.createElement("div");
@@ -43,12 +43,12 @@ function cargarProductosCarrito() {
                     <small>Subtotal</small>
                     <p>$${producto.precio * producto.cantidad}</p>
                 </div>
-                <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
+                <button scr="./img/trash.png" class="carrito-producto-eliminar" id="${producto.id}"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #f00;transform: ;msFilter:;"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg></button>
             `;
     
             contenedorCarritoProductos.append(div);
         })
-    
+
     actualizarBotonesEliminar();
     actualizarTotal();
 	
@@ -57,6 +57,8 @@ function cargarProductosCarrito() {
         contenedorCarritoProductos.classList.add("disabled");
         contenedorCarritoAcciones.classList.add("disabled");
         contenedorCarritoComprado.classList.add("disabled");
+    cantidadProductoCarrito.innerText = '0';
+
     }
 
 }
@@ -94,19 +96,47 @@ function eliminarDelCarrito(e) {
 
     const idBoton = parseInt(e.currentTarget.id);
     const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
-    
-    productosEnCarrito.splice(index, 1);
+    if(productosEnCarrito.length>1){
+        productosEnCarrito.splice(index, 1);
+        localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    }else{
+        if(productosEnCarrito.length===1){
+            productosEnCarrito.pop();
+            localStorage.removeItem("productos-en-carrito");
+        }
+    }    
     cargarProductosCarrito();
 
-    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    
 
 }
 
 botonVaciar.addEventListener("click", vaciarCarrito);
 function vaciarCarrito() {
-
-    productosEnCarrito.length = 0;
-    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    Toastify({
+        text: "Producto eliminado",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          },
+        onClick: function(){} // Callback after click
+      }).showToast();
+    if (productosEnCarrito && productosEnCarrito.length > 0) {
+        productosEnCarrito.length = 0;
+  //      localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+        localStorage.removeItem("productos-en-carrito");
+    }
     cargarProductosCarrito();
 }
 
@@ -114,6 +144,8 @@ function vaciarCarrito() {
 function actualizarTotal() {
     const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
     total.innerText = `$${totalCalculado}`;
+    let contadorProduct = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    contadorProductosCarrito.innerText = contadorProduct;
 }
 
 botonComprar.addEventListener("click", comprarCarrito);
